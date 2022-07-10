74d1ee59d25cd6eba270e42f1f4a57f99f3545c9
Byungjin Park <posquit0.bj@gmail.com>
2022-07-11 02:43:30 +0900

    Add ecr-registry-pull-through-cache example

---
+++ examples/ecr-registry-pull-through-cache/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "this" {}
+
+
+###################################################
+# ECR Registry
+###################################################
+
+module "registry" {
+  source = "../../modules/ecr-registry"
+  # source  = "tedilabs/container/aws//modules/ecr-registry"
+  # version = "~> 0.19.0"
+
+  pull_through_cache_policies = [
+    {
+      # Specify one or more IAM principals to grant permission. Support the ARN of IAM entities, or AWS account ID.
+      iam_entities = [data.aws_caller_identity.this.account_id]
+      allow_create_repository = true
+      repositories = [
+        "ecr-public/amazonlinux/*",
+        "ecr-public/aws-ec2/aws-node-termination-handler",
+        "quay/*",
+      ]
+
+    }
+  ]
+  pull_through_cache_rules    = [
+    {
+      namespace    = "ecr-public"
+      upstream_url = "public.ecr.aws"
+    },
+    {
+      namespace    = "quay"
+      upstream_url = "quay.io"
+    },
+  ]
+
+  tags = {
+    "project" = "terraform-aws-container-examples"
+  }
+}
