1c20890ff68f8acf7ed04a37fe916956a155c00f
Byungjin Park <posquit0.bj@gmail.com>
2022-07-11 02:48:10 +0900

    Add ecr-registry-enhanced-scanning example

---
+++ examples/ecr-registry-enhanced-scanning/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "this" {}
+
+
+###################################################
+# ECR Registry
+###################################################
+
+module "registry" {
+  source = "../../modules/ecr-registry"
+  # source  = "tedilabs/container/aws//modules/ecr-registry"
+  # version = "~> 0.19.0"
+
+  scanning_type               = "ENHANCED"
+  scanning_on_push_filters    = ["quay/*", "sre/*"]
+  scanning_continuous_filters = ["example/example"]
+
+
+  tags = {
+    "project" = "terraform-aws-container-examples"
+  }
+}
---
+++ examples/ecr-registry-enhanced-scanning/outputs.tf
@@ -0,0 +1,3 @@
+output "registry" {
+  value     = module.registry
+}
