11ff8fd9fb92dab7e0f35e532105d403a0ea04db
Byungjin Park <posquit0.bj@gmail.com>
2025-11-17 02:39:01 +0900

    feat(eks-max-pods): update dataset

--- modules/eks-max-pods/variables.tf
+++ modules/eks-max-pods/variables.tf
@@ -1,17 +1,18 @@
 variable "instance_type" {
   description = "(Required) The type of instances to run in the EKS cluster node group."
   type        = string
+  nullable    = false
 }
 
 variable "use_cni_custom_networking" {
-  description = "(Optional) Use EKS CNI Custom Networking."
+  description = "(Optional) Whether to use EKS CNI Custom Networking. Defaults to `true`."
   type        = bool
   default     = true
   nullable    = false
 }
 
 variable "use_cni_eni_prefix_mode" {
-  description = "(Optional) Use ENI Prefix Mode. Defaults to `false`."
+  description = "(Optional) Whether to use ENI Prefix Mode. Defaults to `false`."
   type        = bool
   default     = false
   nullable    = false
